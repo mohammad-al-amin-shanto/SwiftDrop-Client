b82878936adeece98aab91591e77a6367deb869c
alamin-shanto <mdalaminshanto1993@gmail.com>
2025-11-13 15:03:28 +0600

    added the store.ts file

---
+++ src/app/store.ts
@@ -0,0 +1,21 @@
+// src/app/store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "../api/baseApi";
+import authReducer from "../features/auth/authSlice";
+import parcelsReducer from "../features/parcels/parcelsSlice";
+import usersReducer from "../features/users/usersSlice";
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: authReducer,
+    parcels: parcelsReducer,
+    users: usersReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+  devTools: import.meta.env.DEV,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
